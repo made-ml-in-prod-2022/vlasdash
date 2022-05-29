fb63580a3541d34f382c583ee1fdbb1259a91d76
Darya Vlaskina <vlaskinadarja@rambler.ru>
2022-05-30 02:05:20 +0300

    add test for incorrect request data

--- online_inference/test/test.py
+++ online_inference/test/test.py
@@ -1,7 +1,7 @@
 import requests
 
 
-def test_predict():
+def test_predict_correct():
     """Test correct work online inference predictions."""
 
     request = [
@@ -48,3 +48,33 @@ def test_predict():
     assert len(response.json()) == len(request)
     assert response.json()[0]["id"] == request[0]["id"]
     assert response.json()[1]["id"] == request[1]["id"]
+
+
+def test_predict_incorrect_data():
+    """Test work predictions with incorrect thal value."""
+
+    request = [
+      {
+        "id": 0,
+        "age": 69,
+        "sex": 1,
+        "cp": 0,
+        "trestbps": 160,
+        "chol": 234,
+        "fbs": 1,
+        "restecg": 2,
+        "thalach": 131,
+        "exang": 0,
+        "oldpeak": 0.1,
+        "slope": 1,
+        "ca": 1,
+        "thal": 5,
+      },
+    ]
+
+    response = requests.get(
+        "http://0.0.0.0:8000/predict/",
+        json=request,
+    )
+
+    assert response.status_code == 500
